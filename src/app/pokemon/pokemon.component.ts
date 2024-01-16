d50d20ad62b60e6b38e40ad45ec2c597a2c368cf
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2024-01-16 18:59:11 -0300

    fix: error validation

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
+import { catchError, debounceTime, distinctUntilChanged, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
@@ -23,23 +23,30 @@ export class PokemonComponent {
     this.searchForm
       .get('searchQuery')
       .valueChanges.pipe(
-        debounceTime(800), // Wait for 300ms pause in events
+        debounceTime(800), // Wait for 800ms pause in events
         distinctUntilChanged(), // Only emit when the current value is different from the last
         tap(() => (this.loading = true)),
         switchMap(() => {
-          this.loading = true;
-          this.pokemon = null;
           return this.pokemonService.getPokemon(
             this.searchForm.get('searchQuery').value
-            ) }// Make the API call
-          )
+            ).pipe(
+              catchError(() => {
+                return of(null); // Return an empty observable to continue the chain
+              })
+            )
+          }// Make the API call
+        )
       )
       .subscribe((data: any) => {
-        console.log(data);
         if (data) {
           this.pokemon = data;
+        } else {
+          this.pokemon = null;
         }
         this.loading = false;
-      });
+      }),
+      (error: any) => {
+        console.log(error)
+      };
   }
 }

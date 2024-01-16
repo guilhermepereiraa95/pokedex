c314199934effbd603fcae1f0d771402e19089a8
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2024-01-16 17:24:46 -0300

    fix: adding formbuilder and debounce on subscribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,30 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
-  templateUrl: './pokemon.component.html'
+  templateUrl: './pokemon.component.html',
 })
-export class PokemonComponent implements OnInit {
-  pokemonName = 'charizard';
+export class PokemonComponent {
   pokemon: any = [];
+  searchForm: any;
 
-  constructor(private pokemonService: PokemonService) { }
-
-  ngOnInit(): void {
-    this.loadPokemon();
-  }
-
-  loadPokemon(): void {
-    if(this.pokemonName === ""){
-      return;
-    }
-
-    this.pokemonService.getPokemon(this.pokemonName).subscribe(data => {
-      if(data) {
-        this.pokemon = [data];
-      }
+  constructor(
+    private pokemonService: PokemonService,
+    private formBuilder: FormBuilder
+  ) {
+    this.searchForm = this.formBuilder.group({
+      searchQuery: [null],
     });
-  }
 
+    this.searchForm
+      .get('searchQuery')
+      .valueChanges.pipe(
+        debounceTime(1000), // Wait for 300ms pause in events
+        distinctUntilChanged(), // Only emit when the current value is different from the last
+        switchMap(() =>
+          this.pokemonService.getPokemon(
+            this.searchForm.get('searchQuery').value
+          )
+        ) // Make the API call
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+        if (data) {
+          this.pokemon = [data];
+        }
+      });
+  }
 }

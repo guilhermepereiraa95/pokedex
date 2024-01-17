294778d1830e307aca3da888e785407e3f8cccdf
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2024-01-16 23:58:34 -0300

    reafactor: adding refactor changes to logic

--- src/app/pokemon/shared/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/shared/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { PokemonService } from 'src/app/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,4 +8,7 @@ import { Component, Input } from '@angular/core';
 })
 export class PokemonListComponent {
   @Input() pokemonList: any[] = [];
+  constructor(private pokemonService: PokemonService) {
+
+  }
 }

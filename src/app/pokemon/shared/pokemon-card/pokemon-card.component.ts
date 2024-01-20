2c4f3875468f76b54724df2eefad3cd932ffc13b
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2024-01-20 14:03:27 -0300

    fix: adding details logic

--- src/app/pokemon/shared/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon/shared/pokemon-card/pokemon-card.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class PokemonCardComponent {
   @Input() pokemon: any = null;
+  @Input() loading = false;
   pokemonDetails: any = null;
   constructor() { }
 

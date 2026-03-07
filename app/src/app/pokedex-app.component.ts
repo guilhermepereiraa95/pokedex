c404a5b5fb76e034956c6272cfd133b072aeca1c
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2026-03-06 21:36:37 -0300

    feature: configuring Docker images and versions

--- app/src/app/pokedex-app.component.ts
+++ app/src/app/pokedex-app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './pokedex-app.routes';
 
 @Component({
   selector: 'pokedex',
